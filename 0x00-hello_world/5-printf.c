f97b7b4e9c6d5b238b7f79b50bc70c4de3dd6a24
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-13 17:44:41 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The entry points
+ * Description:it prints a specific sentence
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
