76933d42a468d0bcfda0f5144015ec021616238f
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-14 02:26:51 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point function
+ * description: prints numbers from 1 to 9 with space and ,
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			putchar('\n');
+		}
+		else
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
