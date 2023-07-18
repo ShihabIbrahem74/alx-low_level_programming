03df7d77c562e46d42513fa9a3c02456ccbaa7a1
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-18 14:48:02 -0700

    task1 fix8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,10 @@
 
 void print_alphabet(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-                _putchar(i);
-        _putchar('\n');
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
 
 }
