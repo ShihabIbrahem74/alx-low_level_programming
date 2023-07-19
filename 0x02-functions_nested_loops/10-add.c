bc95a67c3975c98e0e205cdebab2250bae7891e6
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-18 18:16:37 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - calls another function called _putchar
+ *
+ * Description: for alx task 0
+ * 
+ * @x: first int to add
+ * @y: second int to add
+ *
+ * Return: 0 on success
+ */
+int add(int x, int y)
+{
+int i;
+
+i = x + y;
+
+return (i);
+}
