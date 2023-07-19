dc68850fc02c0ff71971c1671423f785517676f0
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-18 18:08:50 -0700

    task9 fix2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,15 +18,27 @@ for (rows = 0; rows < 10; rows++)
 
 for (col = 0; col < 10; col++)
 {
+if (col > 0)
+{
 sum = rows * col;
+_putchar(',');
 if (sum <= 9)
+{
+_putchar(' ');
+_putchar(' ');
 _putchar(sum + '0');
+}
 else
 {
+_putchar(' ');
 _putchar((sum / 10) + '0');
 _putchar((sum % 10) + '0');
 }
-_putchar(' ');
+}
+else
+{
+_putchar(0 + '0');
+}
 }
 _putchar('\n');
 }
