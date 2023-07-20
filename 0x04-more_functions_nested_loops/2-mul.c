18e3ba12adaaf8ea94df317237a31b56b80c81a5
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-20 11:43:32 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - calls another function called _putchar
+ *
+ * Description: for alx task 0
+ *
+ * @a: 'a' is int recieved from another function
+ * @b: 'b' is int recieved from another function
+ *
+ * Return: 0 on success
+ */
+
+int mul(int a, int b)
+{
+int i;
+i = a * b;
+return (i);
+}
