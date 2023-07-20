eac69ff5206e664d2918785f087e956eb74b41c3
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-20 12:43:51 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - calls another function called _putchar
+ *
+ * Description: for alx task 0
+ *
+ * @n : 'n' is an int taked from another function
+ *
+ * Return: void on success
+ */
+
+void print_diagonal(int n)
+{
+int space;
+int i;
+
+for (i = 0; i < n; i++)
+{
+for (space = 0; space < i; space++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+if (n == 0)
+_putchar('\n');
+}
