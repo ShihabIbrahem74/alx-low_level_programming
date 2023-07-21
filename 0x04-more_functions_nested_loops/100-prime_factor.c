8eddbcf47044e8aeea4d7652d83065b2e20134d5
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-20 17:32:45 -0700

    adv1 final fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,51 +12,21 @@ int prime_check(int64_t n);
  */
 int main(void)
 {
-int64_t i;
-int64_t final;
-int64_t num = 612852475143;
+long int i;
+long int final;
+long int num = 612852475143;
 
-for (i = 2; i < num; i++)
+while (num % 2 == 0)
+num = num / 2;
+for (i = 3; i <= sqrt(num); i += 2)
 {
-if (prime_check(i))
+while (num % i == 0)
 {
-if (num % i == 0)
-{
-final = i;
 num = num / i;
+final = i;
 }
 }
-}
+if (num > 2)
+final = num;
 printf("%ld\n", final);
-return (0);
-}
-
-/**
- * prime_check - check every prime number
- *
- * Description: for alx task 0
- *
- * @n: 'n' is the prime number to be checked
- *
- * Return: Always ret.
- */
-
-int prime_check(int64_t n)
-{
-int check;
-int ret;
-
-for (check = 2; check <= n; check++)
-{
-if (n % check == 0 && check != n)
-{
-ret = 0;
-break;
-}
-if (check == n)
-{
-ret = 1;
-}
-}
-return (ret);
 }
