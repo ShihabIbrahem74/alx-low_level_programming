2fcb5853356929d2eac206b33eac607f4186efff
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-24 15:16:05 -0700

    adv1

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i;
+char x;
+
+srand(time(NULL));
+while (i <= 2645)
+{
+x = rand() % 128;
+i += x;
+putchar(c);
+}
+putchar(2772 - i);
+return (0);
