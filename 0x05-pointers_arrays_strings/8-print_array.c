6e9d152c33c2a79bd4b481706892ae9323fb5056
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-24 14:10:27 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @a: '*a' is a pointer
+ *
+ * @n: 'n' is an integer
+ *
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n - 1; i++)
+printf("%d, ", a[i]);
+printf("%d\n", a[n - 1]);
+}
