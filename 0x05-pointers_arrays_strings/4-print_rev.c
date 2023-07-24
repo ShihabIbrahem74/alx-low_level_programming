464e333da86a9a90856d0b5e0384e3aaf7c1d69f
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-24 13:19:17 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @s: '*str' is a pointer
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int len = 0;
+while (s[len] != 0)
+len++;
+len--;
+while (s[len] != 0)
+{
+_putchar(s[len]);
+len--;
+}
+}
