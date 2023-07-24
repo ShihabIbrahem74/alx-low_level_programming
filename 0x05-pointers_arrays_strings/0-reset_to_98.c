642db7a7e89bcb330b6ff1858884d8d5a048493b
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-24 11:57:29 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @n: '*n' is a pointer
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Description #
+Tasks for 0x05. C - Pointers, arrays and strings alx project
