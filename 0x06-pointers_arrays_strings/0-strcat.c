cf0f21cad5b72e11f5f94010145d28371d6a124b
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-26 16:55:01 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @dest: '*dest' is a pointer
+ *
+ * @src: '*src' is a pointer
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int length1 = 0;
+int length2 = 0;
+int i = 0;
+
+while (dest[length1] != 0)
+length1++;
+while (src[length2] != 0)
+{
+dest[length1] = src[length2];
+length1++;
+length2++;
+}
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Description #
+Tasks for 0x06. C - Pointers, arrays and strings alx project
