11e6188d74ce8d0cf94ec187d31ff17b0c3666fd
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-07-31 13:50:31 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @s: '*s' is a pointer
+ *
+ * @c: 'n' is an integer
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != 0)
+{
+if (s[i] == c)
+return (s + i);
+i++;
+}
+return (NULL);
+}
