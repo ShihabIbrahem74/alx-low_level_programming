de4f3e810d20cdd6981633ea39df1ba21d533e2f
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-02 17:28:33 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- *  _sqrt_recursion - fucntion that is mintioned in another code
+ *  root - fucntion that is mintioned in another code
  *
  * Description: function to do task for alx
  *
@@ -10,7 +11,31 @@
  *
  * Return: Always 0.
  */
+
 int root(int n, int i);
+
+int root(int n, int i)
+{
+if (i * i == n)
+return (i);
+else if (i * i < n)
+{
+i = root(n, i + 1);
+return (i);
+}
+else
+return (-1);
+}
+
+/**
+ * _sqrt_recursion - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx that is called in another fucntion
+ *
+ * @n: '*s' is a pointer
+ *
+ * Return: Always 0.
+ */
 
 int _sqrt_recursion(int n)
 {
@@ -19,18 +44,3 @@ int result;
 result = root(n, 1);
 return (result);
 }
-
-/**
- * root - fucntion that is mintioned in another code
- *
- * Description: function to do task for alx
- *
- * @n: '*s' is a pointer
- *
- * @i: 'i' is an integer
- *
- * Return: Always 0.
- */
-
-int root(int n, int i)
-{
