cacf79b9918b8b53a172da556d0a58ecd3c5a14d
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-04 14:06:38 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - fucntion to print the file name
+ *
+ * Description: function to do task for alx
+ *
+ * @argc: 'argc' is an integer
+ * @argv: '**argv' is a pointer to array
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+int sum = 0;
+int i, x;
+
+for (i = 0; i < argc - 1; i++)
+{
+x = atoi(argv[i + 1]);
+if (x == 0)
+{
+printf("Error\n");
+return (1);
+}
+else
+sum += x;
+}
+printf("%d\n", sum);
+return (0);
+}
