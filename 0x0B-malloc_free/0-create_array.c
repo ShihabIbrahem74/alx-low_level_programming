2ef8768121eb7d3b0fdcbcee1df6aa4d2adde975
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-07 14:44:35 -0700

    task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - function is called ny another file called main.c
+ *
+ * Description: for alx project malloc
+ *
+ * @size: 'size' is int recieved from another function
+ * @c: 'c' is a char fron another function
+ *
+ * Return: 0 on success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *ptr;
+int i;
+
+if (size > 0)
+{
+ptr = (char *)malloc(size * sizeof(char));
+if (ptr == 0)
+return (NULL);
+else
+{
+for (i = 0; i < size; i++)
+ptr[i] = c;
+return (ptr);
+}
+}
+else
+return (NULL);
+}
