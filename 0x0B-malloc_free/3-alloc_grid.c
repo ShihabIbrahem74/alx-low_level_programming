2eb2ada32f064e439987293a7e5c0ff8aa81d9ce
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-08 15:32:43 -0700

    t3 f2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,8 @@ if (width <= 0 || height <= 0)
 return (NULL);
 
 ptr = (int **)malloc(height * sizeof(int *));
-
+if (ptr == NULL)
+	return (NULL);
 for (i = 0; i < height; i++)
 {
 ptr[i] = (int *)malloc(width * sizeof(int));
