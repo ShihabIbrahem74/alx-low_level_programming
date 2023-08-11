f359b80384bfea5a3cd3f7e7be1a1007c737dce7
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-10 17:03:41 -0700

    t2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function is called ny another file called main.c
+ *
+ * Description: for alx project malloc
+ *
+ * @nmemb: 'size' is int recieved from another function
+ * @size: 'size' is int recieved from another function
+ *
+ * Return: 0 on success
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+return (NULL);
+
+for (i = 0; i < nmemb; i++)
+ptr[i] = 0;
+return (ptr);
+}
