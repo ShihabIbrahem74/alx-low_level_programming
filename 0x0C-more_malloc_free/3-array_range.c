59d7b26bb767bb73a77befb5bf4cbf29e4034840
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-10 17:29:33 -0700

    t3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function is called ny another file called main.c
+ *
+ * Description: for alx project malloc
+ *
+ * @min: 'size' is int recieved from another function
+ * @max: 'size' is int recieved from another function
+ *
+ * Return: 0 on success
+ */
+
+int *array_range(int min, int max)
+{
+int size, *ptr;
+
+if (min > max)
+return (NULL);
+
+size = max - min + 1;
+
+ptr = malloc(sizeof(int) * size);
+if (ptr == NULL)
+return (NULL);
+
+for (int i = 0; i < size; i++)
+ptr[i] = min + i;
+return (ptr);
+}
