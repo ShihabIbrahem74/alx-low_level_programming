cf309e1c05d2cb7d758f7f3da4f091220be03b4c
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-14 13:34:02 -0700

    t2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 /**
  * print_dog - checks if a character is a digit
  *
- * @d: struct parameter
+ * @dog: struct parameter
  *
  * Return: void
  */
@@ -17,7 +17,10 @@ printf("Name: (nil)\n");
 else
 printf("Name: %s\n", (*d).name);
 
+if ((*d).age == '\0')
 printf("Age: (nil)\n");
+else
+printf("Age: %f\n", (*d).age);
 
 if ((*d).owner == NULL)
 printf("Owner: (nil)\n");
