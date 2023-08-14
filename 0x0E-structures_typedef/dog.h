1e4557759da137285eb1d1b7d7f344652aaf2882
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-14 03:51:10 -0700

    t0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a struct of three members
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
