e96251321fc91d8eb557765f65c2f4219efe4673
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-14 13:40:24 -0700

    t2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,13 +20,11 @@ printf("Name: %s\n", (*d).name);
 if ((*d).age == '\0')
 printf("Age: (nil)\n");
 else
-printf("Age: %f\n", (*d).age);
+printf("Age: %0.6f\n", (*d).age);
 
 if ((*d).owner == NULL)
 printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", (*d).owner);
 }
-else
-printf("\n");
 }
