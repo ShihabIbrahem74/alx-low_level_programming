556e6c10a24bafc86c3b221b2eb9b6411e0f36a0
ShihabIbrahem74 <shihabibrahem74@gmail.com>
2023-08-14 13:05:59 -0700

    t1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - checks if a character is a digit
+ *
+ * @d: struct parameter
+ * @name: char parameter
+ * @age: float parameter
+ * @owner: char parameter
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
