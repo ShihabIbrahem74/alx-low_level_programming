c0993d5cb26b1407ba2cc6fb5ae097e526b51220
root <root@ShihabComputer.localdomain>
2023-08-17 00:26:13 +0300

    t1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+/**
+ * array_iterator - function for alx project
+ * @array: paprameter one given to the function
+ * @size: parameter two givin to the function
+ * @action: parameter three given to the function
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    int i;
+
+    if (array)
+    {
+        for (i = 0; i < (int)size; i++)
+        {
+            action(i);
+        } 
+    }
+}
