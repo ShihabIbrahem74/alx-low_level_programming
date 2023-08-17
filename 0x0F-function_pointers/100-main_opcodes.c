cabe96e7f16858be1c4259dcef47beea361f26b4
root <root@ShihabComputer.localdomain>
2023-08-17 05:59:43 +0300

    t5

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: counter of arguments
+ * @argv: values of argumens
+ * Return: Always 0.
+ */
+int main (int argc, char **argv)
+{
+	char *p = (char *)main;
+	int a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+}
