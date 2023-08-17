92b900191b68ccb53e2f1aeec74062cf2f530484
root <root@ShihabComputer.localdomain>
2023-08-18 00:38:02 +0300

    t2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (i != (n - 1) && separator)
 			printf("%s", separator);
 		}
-		printf("\n");
 	}
+	printf("\n");
 	va_end(ptr);
 }
