3a2dc417086c648d502b8e8c25e41e2e2744c65f
root <root@ShihabComputer.localdomain>
2023-08-17 21:04:20 +0300

    t0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - check the code
+ *
+ * @n: the count of arguments passed
+ *
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ptr;
+	int i, sum = 0;
+
+	va_start(ptr, n);
+	if (n)
+	{
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(ptr, int);
+		}
+		return (sum);
+	}
+	return (0);
+}
