dc56a8aa7651e2f65ad84f5bf659116ca09aea4e
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-25 02:23:34 +0300

    task3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * _strlen - calulate the length of string
+ * Description: it calulate the string passed to _puts
+ * @s: '*s' is a pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * add_node_end - function that prints
+ * Description: function that prints all the elements
+ * @head: an argument
+ * @str: an argument
+ * Return: 0 always
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	list_t *current;
+
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = _strlen((char *)str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+	*head = new;
+	return (*head);
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
+	return (new);
+}
