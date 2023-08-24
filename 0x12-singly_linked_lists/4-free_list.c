f267dd05fe9c078c9b3aeef3850d26438d5f49de
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-25 02:44:28 +0300

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - function that prints
+ * Description: function that prints all the elements
+ * @head: an argument
+ * Return: 0 always
+*/
+void free_list(list_t *head)
+{
+	list_t *buffer = head;
+	list_t *holder;
+
+	while (buffer->next != NULL)
+	{
+		holder = buffer;
+		buffer = buffer->next;
+		free(holder);
+	}
+	free(buffer);
+}
