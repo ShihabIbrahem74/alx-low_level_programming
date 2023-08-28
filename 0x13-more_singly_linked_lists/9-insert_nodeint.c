f3e9c8694dd9a53450161c6e774e049bed887f22
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-28 23:58:34 +0300

    task9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function that prints
+ * Description: function that prints all the elements
+ * @head: an argument
+ * @idx: an argument
+ * @n: an argument
+ * Return: 0 always
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current = *head;
+	listint_t *new;
+	listint_t *buffer;
+	unsigned int i = 0;
+
+	while (i != (idx - 1))
+	{
+		if (!current)
+		return (NULL);
+		current = current->next;
+		i++;
+	}
+		new = malloc(sizeof(listint_t));
+		if (!new)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->n = n;
+		new->next = current->next;
+		current->next = new;
+		return (new);
+}
