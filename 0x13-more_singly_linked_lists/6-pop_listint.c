8e271cf89eb0e25349ba54417625d26c6996be6b
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-28 16:43:05 +0300

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * pop_listint - function that prints
+ * Description: function that prints all the elements
+ * @head: an argument
+ * Return: 0 always
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int buffer;
+
+	if (!*head || !head)
+	{
+
+		return (0);
+	}
+	buffer = (*head)->n;
+	ptr = (*head)->next;
+	if (!(*head)->next)
+	{
+	ptr = NULL;
+	}
+	free(*head);
+	*head = ptr;
+	return (buffer);
+}
