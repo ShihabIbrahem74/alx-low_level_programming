8de3bf691f313e794a941b23f146be0e70bffa7e
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-31 21:31:59 +0300

    task3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit - function for 0x014 bit project
+ * Description: function for 0x14 bit project
+ * @n: an argument
+ * @index: an argument
+ * Return: 0 always
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	return (-1);
+
+	*n = *n | 1 << index;
+	return (1);
+}
