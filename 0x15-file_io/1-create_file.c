3faea63f0a35a4988f8b0da394ecbadbbf9c80f8
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-09-05 05:46:31 +0300

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,16 +28,15 @@ return (len);
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t flag = 0;
-	ssize_t len = _strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
-		flag = write(fd, text_content, len);
+		bytes = write(fd, text_content, len);
 	close(fd);
-	return (flag == len ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
