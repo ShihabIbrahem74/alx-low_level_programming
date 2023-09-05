6a3941159e7b832be2c2e17398dab78dae72c8fa
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-09-05 05:52:23 +0300

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,47 +1,42 @@
 #include "main.h"
 
 /**
- * _strlen - fucntion that is mintioned in another code
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Description: function to do task for alx
- *
- * @s: '*s' is a pointer
- *
- * Return: Always 0.
+ * Return: integer length of string
  */
-
 int _strlen(char *s)
 {
-int len = 0;
-while (s[len] != 0)
-len++;
-return (len);
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * append_text_to_file - function for alx file I/O task
- * @filename: an argument
- * @text_content: an argument
+ * append_text_to_file - appends text to a file
+ * @filename: name of file to create
+ * @text_content: text to write
  *
- * Return: number bytes read/printed
+ * Return: 1 on success 0 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t flag = 0;
-	ssize_t len = _strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
-	return (-1);
+		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-	return (-1);
+		return (-1);
 	if (len)
-	flag = write(fd, text_content, len);
+		bytes = write(fd, text_content, len);
 	close(fd);
-	if (flag == len)
-	return (1);
-	else
-	return (-1);
+	return (bytes == len ? 1 : -1);
 }
