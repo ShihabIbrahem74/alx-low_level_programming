767f5ca25aff54f6e9362f87cf008e325caa1e4b
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-09-05 07:58:23 +0300

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t flag = 0;
 	ssize_t len = _strlen(text_content);
 
-	if (!filename || !text_content)
+	if (!text_content)
 	return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
