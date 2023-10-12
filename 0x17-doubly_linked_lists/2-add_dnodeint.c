50e4a5ba6809a36e10e5a9ae64a81e1886b047d2
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-10-13 02:08:05 +0300

    task two

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_dnodeint - for alx task
+ * @head: an argument
+ * @n: an argument
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (!*head || !head)
+	{
+		*head = new;
+		new->prev = NULL;
+		new->next = NULL;
+		new->n = n;
+		return (new);
+	}
+	else
+	{
+		new->prev = NULL;
+		new->next = *head;
+		new->n = n;
+		*head = new;
+	}
+	return (new);
+}
