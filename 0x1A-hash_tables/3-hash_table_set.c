6c5075b348e17f8a22b44e5a4c38d1c9362cfb05
root <root@ShihabComputer.localdomain>
2023-11-04 04:05:20 +0200

    task three

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,8 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
-	index = hash_djb2(key) % (ht->size);
-	searcher = ht->array;
+	index = hash_djb2((const unsigned char *)key) % (ht->size);
+	searcher = ht->array[index];
 	while (searcher != NULL)
 	{
 		if (strcmp(searcher->key, key) == 0)
