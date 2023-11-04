301b17c2e35b194d88e4edca41424ee7b9e375ed
root <root@ShihabComputer.localdomain>
2023-11-04 04:34:44 +0200

    task five

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - for alx task 0x1A. C - Hash tables
+ * Description: for alx task 0
+ * @ht: an argument
+ * Return: 0 always
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *searcher;
+	int flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		searcher = ht->array[i];
+		while (searcher != NULL)
+		{
+			if (flag == 0)
+			{
+				printf("'%s' : '%s'", searcher->key, searcher->value);
+				flag = 1;
+			}
+			else
+			{
+				printf(", '%s' : '%s'", searcher->key, searcher->value);
+				searcher = searcher->next;
+			}
+		}
+	}
+	printf("}\n");
+}
