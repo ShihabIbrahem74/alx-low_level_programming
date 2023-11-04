718d7d42d7c8ae557901cf5b239d656a424879b5
root <root@ShihabComputer.localdomain>
2023-11-04 04:03:45 +0200

    task three

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,31 +10,67 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	char *new_value;
 	hash_node_t *new_node;
-	char *temp_key = strdup(key);
-	char *temp_value = strdup(value);
+	hash_node_t *searcher;
 
-	if (!temp_key || temp_value)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
-	if (!ht || !key)
-		return (0);
-
-	index = hash_djb2((const unsigned char *)temp_key) % ht->size;
-	new_node = malloc(sizeof(hash_node_t));
+	index = hash_djb2(key) % (ht->size);
+	searcher = ht->array;
+	while (searcher != NULL)
+	{
+		if (strcmp(searcher->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (!new_value)
+				return (0);
+			free(searcher->value);
+			searcher->value = new_value;
+			return (1);
+		}
+		searcher = searcher->next;
+	}
+	new_node = node_maker(key, value);
 	if (!new_node)
 		return (0);
-	new_node->next = NULL;
-
-	if (ht->array[index])
-	{
-		new_node->next = ht->array[index];
-	}
-	new_node->key = temp_key;
-	new_node->value = temp_value;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+	return (0);
 
-	free(temp_key);
-	free(temp_value);
-	return (1);
+}
+/**
+ * node_maker - for alx task 0x1A. C - Hash tables
+ * Description: for alx task 0
+ * @key: an argument
+ * @value: an argument
+ * Return: 0 always
+*/
+hash_node_t *node_maker(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
 }
